76b80d4b06ac23e73233e711add718860cac8775
Hans <sterionix25@gmail.com>
2022-03-23 20:55:10 +0700

    Update Tugas

--- MyView/app/src/main/java/com/example/myview/MainActivity.java
+++ MyView/app/src/main/java/com/example/myview/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.myview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_1, btn_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
